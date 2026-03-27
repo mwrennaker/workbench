57669cf5c6d97b6ea6257f9251fc955ad264f5af
Martin <mwrennaker@gmail.com>
2026-03-27 16:59:06 -0500

    allow unfree

--- nixos/sunflake/modules/hosts/sunvarPC/configuration.nix
+++ nixos/sunflake/modules/hosts/sunvarPC/configuration.nix
@@ -58,6 +58,7 @@
     enable = true;
     #xwayland.enable = false; one can dream
     #excludePackages = [ "" ];
+    home-manager.url = "https://github.com/nix-community/home-manager/archive/master.tar.gz";
   };
 */
   # Enable CUPS to print documents.
@@ -131,6 +132,7 @@
   # List packages installed in system profile. To search, run:
   environment.systemPackages = with pkgs; [
     vim
+    home-manager.url = "https://github.com/nix-community/home-manager/archive/master.tar.gz";
     obsidian
     git
     signal-desktop
@@ -139,15 +141,7 @@
     vintagestory
  ];
 
-  nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (pkgs.lib.getName pkg) [
-             "obsidian"
-             "steam"
-             "steam-original"
-             "steam-unwrapped"
-             "steam-run"
-             "vintagestory"
-           ];
-
+ nixpkgs.config.allowUnfree = true;
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
@@ -173,6 +167,7 @@
   # this value at the release version of the first install of this system.
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+    home-manager.url = "https://github.com/nix-community/home-manager/archive/master.tar.gz";
   system.stateVersion = "25.11"; # Did you read the comment?
 
 };
