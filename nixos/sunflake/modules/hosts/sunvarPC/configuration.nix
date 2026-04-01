218e91b1b7f5b99b01dc08c46d515fa459f94287
Martin <mwrennaker@gmail.com>
2026-04-01 17:02:37 -0500

    adding steam back lol

--- nixos/sunflake/modules/hosts/sunvarPC/configuration.nix
+++ nixos/sunflake/modules/hosts/sunvarPC/configuration.nix
@@ -1,47 +1,60 @@
-{ self, inputs, ... }: {
-
-  flake.nixosModules.sunvarPCConfiguration = 
-{ pkgs, ... }:
-
+{ self, inputs, ... }:
 {
-  imports =
-    [
-      self.nixosModules.commonConfig
-      self.nixosModules.sunvarPCHardware
-    ];
 
-  boot.kernelPackages = pkgs.linuxPackages_latest;
-  boot.kernelParams = [ "amdgpu.dc=1" "amdgpu.color_range=0" "amdgpu.deep_color=0" ]; #hopefully a workaround to help my monitor for now
+  flake.nixosModules.sunvarPCConfiguration =
+    { pkgs, ... }:
 
-  networking.hostName = "sunvarPC"; # Define your hostname.
- 
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.sunvar = {
-    isNormalUser = true;
-    description = "Martin";
-    extraGroups = [ "networkmanager" "wheel" ];
-  };
+    {
+      imports = [
+        self.nixosModules.commonConfig
+        self.nixosModules.sunvarPCHardware
+        self.nixosModules.steam
+      ];
 
-  environment.systemPackages = with pkgs; [
-    vintagestory
-  ];
+      boot.kernelPackages = pkgs.linuxPackages_latest;
+      boot.kernelParams = [
+        "amdgpu.dc=1"
+        "amdgpu.color_range=0"
+        "amdgpu.deep_color=0"
+      ]; # hopefully a workaround to help my monitor for now
 
-  home-manager.extraSpecialArgs = { 
-    inherit inputs;
-    noctaliaConfig = ../../features/noctalia-sunvarPC.json;
-    outputs = {
-      "DP-2" = { position = "0 0"; scale = "2"; };
-      "HDMI-A-2" = { position = "1920 0"; };
+      networking.hostName = "sunvarPC"; # Define your hostname.
+
+      # Define a user account. Don't forget to set a password with ‘passwd’.
+      users.users.sunvar = {
+        isNormalUser = true;
+        description = "Martin";
+        extraGroups = [
+          "networkmanager"
+          "wheel"
+        ];
+      };
+
+      environment.systemPackages = with pkgs; [
+        vintagestory
+      ];
+
+      home-manager.extraSpecialArgs = {
+        inherit inputs;
+        noctaliaConfig = ../../features/noctalia-sunvarPC.json;
+        outputs = {
+          "DP-2" = {
+            position = "0 0";
+            scale = "2";
+          };
+          "HDMI-A-2" = {
+            position = "1920 0";
+          };
+        };
+        homeModules = [
+          self.homeModules.de
+          self.homeModules.noctalia
+          self.homeModules.nvim
+        ];
+      };
+
+      home-manager.users.sunvar = import ../../../home/sunvar.nix;
+
     };
-    homeModules = [ 
-      self.homeModules.de
-      self.homeModules.noctalia
-      self.homeModules.nvim
-    ];
-  };
-
-  home-manager.users.sunvar = import ../../../home/sunvar.nix;
-
-};
 
 }
