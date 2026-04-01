61f207293151ed4015e32e4255fa46b66cd379d9
Martin <mwrennaker@gmail.com>
2026-04-01 15:38:28 -0500

    more stylix config

--- nixos/sunflake/modules/features/stylix.nix
+++ nixos/sunflake/modules/features/stylix.nix
@@ -8,7 +8,12 @@
         enable = true;
         base16Scheme = "${pkgs.base16-schemes}/share/themes/oxocarbon-dark.yaml";
         targets.gtk.enable = true;
-        targets.neovim.enable = true;
+        targets.neovim.enable = {
+          transparentBackground = {
+            main = true;
+            signColumn = true;
+          };
+        };
         polarity = "dark";
         cursor = {
           package = pkgs.bibata-cursors;
