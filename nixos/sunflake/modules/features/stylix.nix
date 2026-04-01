c1d4f9a18a5e430c6c55ece2caed2a0afec13743
Martin <mwrennaker@gmail.com>
2026-04-01 15:35:22 -0500

    making nvim follow stylix colors

--- nixos/sunflake/modules/features/stylix.nix
+++ nixos/sunflake/modules/features/stylix.nix
@@ -1,17 +1,21 @@
-{ inputs, self, ... }: {
-  flake.nixosModules.stylix = { pkgs, ... }: {
-    imports = [ inputs.stylix.nixosModules.stylix ];
-    stylix = {
-      enable = true;
-      base16Scheme = "${pkgs.base16-schemes}/share/themes/oxocarbon-dark.yaml";
-      targets.gtk.enable = true;
-      polarity = "dark";
-      cursor = {
-	package = pkgs.bibata-cursors;
-	name = "Bibata-Modern-Classic";
-	size = 28;
+{ inputs, self, ... }:
+{
+  flake.nixosModules.stylix =
+    { pkgs, ... }:
+    {
+      imports = [ inputs.stylix.nixosModules.stylix ];
+      stylix = {
+        enable = true;
+        base16Scheme = "${pkgs.base16-schemes}/share/themes/oxocarbon-dark.yaml";
+        targets.gtk.enable = true;
+        targets.neovim.enable = true;
+        polarity = "dark";
+        cursor = {
+          package = pkgs.bibata-cursors;
+          name = "Bibata-Modern-Classic";
+          size = 28;
+        };
       };
     };
-  };
 
 }
