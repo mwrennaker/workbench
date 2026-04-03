4ec85a32c6026e3f3bfe8ebe8b3dd80c5fe7e8d7
Martin <mwrennaker@gmail.com>
2026-04-03 16:27:15 -0500

    forcing steam scaling env var to be forced to 1 hopefully

--- nixos/sunflake/modules/features/steam.nix
+++ nixos/sunflake/modules/features/steam.nix
@@ -1,10 +1,16 @@
-{ inputs, self, ... }: {
-  flake.nixosModules.steam = { ... }: {
+{ inputs, self, ... }:
+{
+  flake.nixosModules.steam =
+    { ... }:
+    {
       programs.steam = {
-      enable = true;
-      remotePlay.openFirewall = true;
-      dedicatedServer.openFirewall = true;
-      localNetworkGameTransfers.openFirewall = true;
+        enable = true;
+        remotePlay.openFirewall = true;
+        dedicatedServer.openFirewall = true;
+        localNetworkGameTransfers.openFirewall = true;
+      };
+      environment.sessionVariables = {
+        STEAM_FORCE_DESKTOPUI_SCALING = "1";
+      };
     };
-  };
 }
