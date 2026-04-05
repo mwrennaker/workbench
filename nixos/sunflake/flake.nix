77940419c33a35e044f4f0d4a23b257606d512f4
Martin <mwrennaker@proton.me>
2026-04-05 01:49:55 -0500

    adding latitude hardware specific configs hopefully

--- nixos/sunflake/flake.nix
+++ nixos/sunflake/flake.nix
@@ -5,18 +5,20 @@
     flake-parts.url = "github:hercules-ci/flake-parts";
     import-tree.url = "github:vic/import-tree";
 
-    home-manager.url = "github:nix-community/home-manager"; 
+    home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
     noctalia = {
       url = "github:noctalia-dev/noctalia-shell";
       inputs.nixpkgs.follows = "nixpkgs";
-     };
+    };
 
     stylix.url = "github:danth/stylix";
     stylix.inputs.nixpkgs.follows = "nixpkgs";
 
-   };
+    nixos-hardware.url = "github:NixOS/nixos-hardware/master";
 
-  outputs = inputs: inputs.flake-parts.lib.mkFlake {inherit inputs;} (inputs.import-tree ./modules);
+  };
+
+  outputs = inputs: inputs.flake-parts.lib.mkFlake { inherit inputs; } (inputs.import-tree ./modules);
 }
--- nixos/sunflake/modules/hosts/comet/default.nix
+++ nixos/sunflake/modules/hosts/comet/default.nix
@@ -1,14 +1,23 @@
-{ self, inputs, ... }: {
+{
+  self,
+  inputs,
+  nixos-hardware,
+  ...
+}:
+{
 
   flake.nixosConfigurations.comet = inputs.nixpkgs.lib.nixosSystem {
 
-    specialArgs = { inherit inputs self; primaryUser = "comet"; };
+    specialArgs = {
+      inherit inputs self;
+      primaryUser = "comet";
+    };
 
-    modules = [ 
+    modules = [
       self.nixosModules.cometConfiguration
-    ];  
+      nixos-hardware.nixosModules.dell-latitude-7390
+    ];
 
   };
-  
 
 }
