9a592a39eb75d12d098eb3b62666c32a301c532b
Martin <mwrennaker@gmail.com>
2026-04-10 22:47:29 -0500

    gtk4 theme in home conf

--- nixos/sunflake/home/comet.nix
+++ nixos/sunflake/home/comet.nix
@@ -1,8 +1,16 @@
-{ pkgs, homeModules, ... }: {
+{
+  pkgs,
+  config,
+  homeModules,
+  ...
+}:
+{
 
   imports = homeModules;
- 
+
   home.username = "comet";
   home.homeDirectory = "/home/comet";
   home.stateVersion = "25.11";
+
+  gtk.gtk4.theme = config.gtk.theme;
 }
